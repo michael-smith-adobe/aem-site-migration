872b82a3793a20fb274d097ed5ec4451c07e0fe0
Michael Smith <michasmi@adobe.com>
2026-04-27 18:00:55 -0700

    feat: walgreens migration

---
+++ blocks/featured-categories/featured-categories.js
@@ -0,0 +1,23 @@
+import { createOptimizedPicture } from '../../scripts/aem.js';
+
+export default function decorate(block) {
+  const ul = document.createElement('ul');
+  [...block.children].forEach((row) => {
+    const li = document.createElement('li');
+    while (row.firstElementChild) li.append(row.firstElementChild);
+    [...li.children].forEach((div) => {
+      if (div.children.length === 1 && div.querySelector('picture')) {
+        div.className = 'featured-categories-image';
+      } else {
+        div.className = 'featured-categories-label';
+      }
+    });
+    ul.append(li);
+  });
+  ul.querySelectorAll('picture > img').forEach((img) => {
+    img.closest('picture').replaceWith(
+      createOptimizedPicture(img.src, img.alt, false, [{ width: '200' }]),
+    );
+  });
+  block.replaceChildren(ul);
+}
