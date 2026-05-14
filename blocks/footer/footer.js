6c1a48d0dd12f225b019f8ac04257804ab110117
Michael Smith <michasmi@adobe.com>
2026-05-14 00:30:14 -0500

    fix(footer): use path-prefix for multi-site footer routing

--- blocks/footer/footer.js
+++ blocks/footer/footer.js
@@ -8,7 +8,14 @@ import { loadFragment } from '../fragment/fragment.js';
 export default async function decorate(block) {
   // load footer as fragment
   const footerMeta = getMetadata('footer');
-  const footerPath = footerMeta ? new URL(footerMeta, window.location).pathname : '/footer';
+  let footerPath;
+  if (footerMeta) {
+    footerPath = new URL(footerMeta, window.location).pathname;
+  } else {
+    // Default: use site-relative footer based on the current path prefix
+    const pathParts = window.location.pathname.split('/').filter(Boolean);
+    footerPath = pathParts.length > 0 ? `/${pathParts[0]}/footer` : '/footer';
+  }
   const fragment = await loadFragment(footerPath);
 
   // decorate footer DOM
